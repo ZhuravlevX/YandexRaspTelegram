9be0aef0b545c90f3855a139af2c050006d7e8ac
ZhuravlevX <126952798+ZhuravlevX@users.noreply.github.com>
2025-01-04 15:44:13 +0300

    Added new module “Pydantic” to the file and added module versions

--- module.bat
+++ module.bat
@@ -1,10 +1,11 @@
 @echo off
 
-pip install aiogram
-pip install requests
-pip install babel
-pip install pytz
-pip install python-dotenv
+pip install aiogram==3.17.0
+pip install requests==2.32.3
+pip install babel==2.16.0
+pip install pytz==2024.2
+pip install python-dotenv==1.0.1
+pip install pydantic==2.10.4
 
 :cmd
 pause null
