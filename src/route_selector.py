b99371977e7c2944235db418c4f0d763ffdd9614
ZhuravlevX <126952798+ZhuravlevX@users.noreply.github.com>
2025-01-06 04:54:48 +0300

    Added design to text responses

--- src/route_selector.py
+++ src/route_selector.py
@@ -26,21 +26,21 @@ def select_stations_list(stations: list[Station]) -> str:
 @route_selector.callback_query(lambda c: c.data == 'find_route')
 async def find_route(c: CallbackQuery, state: FSMContext):
     await state.set_state(RouteSelectState.from_station_search)
-    await c.message.reply('Введите название станции отправления:')
+    await c.message.reply('🚆🛃 <b>Введите название станции или платформы ОТКУДА вы отправляетесь</b>', parse_mode='HTML')
 
 
 @route_selector.message(RouteSelectState.from_station_search)
 async def from_station_handler(message: Message, state: FSMContext):
     stations = find_station(message.text.casefold())
     if len(stations) == 0:
-        await message.reply('Такой станции не найдено')
+        await message.reply(f"❌🛃 <b>Станция не найдена. Пожалуйста, укажите корректное название станции.</b>", parse_mode='HTML')
     elif len(stations) == 1:
         await message.reply(
-            f'Выбрана станция отправления:\n{stations[0].title} ({stations[0].region})\n\nВведите название конечной станции:')
+            f'🚆🛃 <b>Найдена станция "{stations[0].title}" в {stations[0].region}. Введите название станции или платформы КУДА вы едете.</b>', parse_mode='HTML')
         await state.update_data(from_station=stations[0].code)
         await state.set_state(RouteSelectState.to_station_search)
     else:
-        await message.reply(f'Найдены станции:\n{select_stations_list(stations)}\n\nВведите номер нужной станции:')
+        await message.reply(f'🚆🛃 <b>Найдены следующие станции с похожим названием:\n{select_stations_list(stations)}\n\nВведите номер нужной вам станции.</b>', parse_mode='HTML')
         await state.set_state(RouteSelectState.from_station_second_search)
         await state.update_data(stations_list=stations)
 
@@ -53,11 +53,11 @@ async def from_station_second_search_handler(message: Message, state: FSMContext
         if index > 10 or index < 1:
             raise Exception
     except:
-        return await message.reply('Неверный номер станции')
+        return await message.reply('f❌🛃 <b>Неверный номер станции</b>', parse_mode='HTML')
     data = await state.get_data()
     station: Station = data['stations_list'][index - 1]
     await message.reply(
-        f'Выбрана станция отправления:\n{station.title} ({station.region})\n\nВведите название конечной станции:')
+        f'🚆🛃 <b>Выбрана станция "{station.title}" ({station.region}). Введите название станции или платформы КУДА вы едете.</b>', parse_mode='HTML')
     await state.update_data(from_station=station.code)
     await state.set_state(RouteSelectState.to_station_search)
 
@@ -66,15 +66,15 @@ async def from_station_second_search_handler(message: Message, state: FSMContext
 async def to_station_handler(message: Message, state: FSMContext):
     stations = find_station(message.text.casefold())
     if len(stations) == 0:
-        await message.reply('Такой станции не найдено')
+        await message.reply(f'❌🛃 <b>Станция не найдена. Пожалуйста, укажите корректное название станции.</b>', parse_mode='HTML')
     elif len(stations) == 1:
         await message.reply(
-            f'Выбрана конечная станция:\n{stations[0].title} ({stations[0].region})')
+            f'🚆🛃 <b>Найдена станция "{stations[0].title}" ({stations[0].region}). Маршрут был установлен.</b>', parse_mode='HTML')
         await state.update_data(to_station=stations[0].code)
         await state.set_state()
 
     else:
-        await message.reply(f'Найдены станции:\n{select_stations_list(stations)}\n\nВведите номер нужной станции:')
+        await message.reply(f'🚆🛃 <b>Найдены следующие станции с похожим названием:\n{select_stations_list(stations)}\n\nВведите номер нужной вам станции.</b>', parse_mode='HTML')
         await state.set_state(RouteSelectState.to_station_second_search)
         await state.update_data(stations_list=stations)
 
@@ -87,9 +87,9 @@ async def to_station_second_search_handler(message: Message, state: FSMContext):
         if index > 10 or index < 1:
             raise Exception
     except:
-        return await message.reply('Неверный номер станции')
+        return await message.reply(f'❌🛃 <b>Неверный номер станции</b>', parse_mode='HTML')
     data = await state.get_data()
     station: Station = data['stations_list'][index - 1]
-    await message.reply(f'Выбрана конечная станция:\n{station.title} ({station.region})')
+    await message.reply(f'🚆🛃 <b>Найдена станция "{station.title}" ({station.region}). Маршрут был установлен.</b>', parse_mode='HTML')
     await state.update_data(to_station=station.code)
     await state.set_state()
