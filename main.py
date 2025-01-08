ebd8c0312a164dcbe854d1175f527307c64cd67e
KryptonFox <kirieshka0785@gmail.com>
2025-01-09 01:37:25 +0300

    Autoupdate tracking with states

--- main.py
+++ main.py
@@ -28,26 +28,25 @@ dp.include_router(route_selector)
 
 logging.basicConfig(level=logging.INFO)
 
-auto_update_users = {}
-current_messages = {}
 
-
-async def update_trains(message: Message, user_id: int, from_station: str, to_station: str):
-    global auto_update_users, current_messages
+async def update_trains(message: Message, state: FSMContext):
     remaining_time = 60
-    auto_update_users[user_id] = True
-    current_messages[user_id] = message
+    data = await state.get_data()
+    from_station = data.get('from_station')
+    to_station = data.get('to_station')
+    await state.update_data(autoupdate=True)
 
     for i in range(60):
         current_time = datetime.now().strftime('%H:%M')
         train_info = get_train_info(from_station, to_station)
         random_image = random.choice(image_urls)
+        data = await state.get_data()
 
-        if not auto_update_users[user_id]:
+        if not data.get("autoupdate"):
             train_info += f"\n🚆🚫<b> Автообновление было отменено. Последние данные были обновлены в {current_time}.</b>"
             media = InputMediaPhoto(media=random_image, caption=train_info, parse_mode='HTML')
             await message.edit_media(media)
-            auto_update_users[user_id] = False
+            await state.update_data(autoupdate=False)
             return
 
         if train_info:
@@ -55,11 +54,11 @@ async def update_trains(message: Message, user_id: int, from_station: str, to_st
                 remaining_time -= 1
                 additional_text = f"\n<b>🚆⌛ Следующее обновление через 1 минуту. Оставшееся время обновления: {remaining_time} минут.</b>"
                 keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(
-                    text="🚫 | Отменить автообновление", callback_data=f"cancel_update_{user_id}")]])
+                    text="🚫 | Отменить автообновление", callback_data=f"cancel_update")]])
 
             else:
                 additional_text = f"\n<b>🚆⌛ Автообновление было завершено в {current_time}, учтите актуальность данного расписания!</b>"
-                auto_update_users[user_id] = False
+                await state.update_data(autoupdate=False)
                 keyboard = None
 
             train_info += additional_text
@@ -70,17 +69,15 @@ async def update_trains(message: Message, user_id: int, from_station: str, to_st
                 "🚆🚫 <b>На текущий момент, на данную дату или заданному маршруту мы не нашли расписание. "
                 "Пожалуйста, укажите текущую или же дальнейшую дату для расписания и проверьте, правильно ли вы указали названия станций для маршрута.</b>",
                 parse_mode='HTML')
-            auto_update_users[user_id] = False
+            await state.update_data(autoupdate=False)
         await asyncio.sleep(60)
 
 
 @dp.message(CommandStart())
 async def send_welcome(message: Message):
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="⬅ | Указать маршрут", callback_data="find_route")],
-                           [InlineKeyboardButton(text="🚆 | Расписание электричек", callback_data="send_suburban")]])
-        # inline_keyboard=[[InlineKeyboardButton(text="⬅ | Указать маршрут", callback_data="find_route"),
-        #                   InlineKeyboardButton(text="🇷🇺 | Выбрать язык", callback_data="lang")],[InlineKeyboardButton(text="🚆 | Расписание электричек", callback_data="send_suburban")]])
-
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[[InlineKeyboardButton(text="⬅ | Указать маршрут", callback_data="find_route")],
+                         [InlineKeyboardButton(text="🚆 | Расписание электричек", callback_data="send_suburban")]])
 
     random_image = random.choice(image_urls)
     await message.answer_photo(photo=random_image,
@@ -90,23 +87,19 @@ async def send_welcome(message: Message):
                                parse_mode='HTML', reply_markup=keyboard)
 
 
-async def send_trains(message: Message, from_station, to_station):
-    global auto_update_users, current_messages
-    user_id = message.from_user.id
-    if auto_update_users.get(user_id, False):
+async def send_trains(message: Message, state: FSMContext):
+    if (await state.get_data()).get("autoupdate"):
         await message.reply("🚆📋 <b>Расписание с автообновление на данный момент активно. "
                             "Пожалуйста, отключите текущее автообновление перед запуском нового расписания.</b>",
                             parse_mode='HTML')
         return
     initial_message = await message.reply("🚆📋 <b>Получаем расписание поездов...</b>", parse_mode='HTML')
-    await update_trains(initial_message, user_id, from_station, to_station)
+    await update_trains(initial_message, state)
 
 
-@dp.callback_query(lambda c: c.data.startswith('cancel_update_'))
-async def cancel_update(callback_query: types.CallbackQuery):
-    global auto_update_users
-    user_id = int(callback_query.data.split('_')[-1])
-    auto_update_users[user_id] = False
+@dp.callback_query(lambda c: c.data == 'cancel_update')
+async def cancel_update(callback_query: types.CallbackQuery, state: FSMContext):
+    await state.update_data(autoupdate=False)
     await bot.answer_callback_query(callback_query.id,
                                     text="🚫⌛ Автообновление было отменено, учтите что данные могут быть неактуальными. Отмена произойдет в течении минуты.")
     await bot.edit_message_reply_markup(callback_query.message.business_connection_id, callback_query.message.chat.id,
@@ -116,10 +109,7 @@ async def cancel_update(callback_query: types.CallbackQuery):
 
 @dp.callback_query(lambda c: c.data == "send_suburban")
 async def handle_send_suburban(callback_query: types.CallbackQuery, state: FSMContext):
-    data = await state.get_data()
-    from_station = data.get('from_station', 's2000003')
-    to_station = data.get('to_station', 's9600786')
-    await send_trains(callback_query.message, from_station, to_station)
+    await send_trains(callback_query.message, state)
 
 
 if __name__ == '__main__':
