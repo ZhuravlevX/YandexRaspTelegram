96a43b6b5dbc38a5851cd4a2df36584ec93188cd
KryptonFox <kirieshka0785@gmail.com>
2025-01-09 02:02:07 +0300

    Migrate config to json5 format

--- src/utils/load_config.py
+++ src/utils/load_config.py
@@ -1,9 +1,9 @@
-import json
+import json5
 
 from src.models.config import Config
 
 
 def load_config() -> Config:
-    with open('config.json', 'r', encoding='utf-8') as config_file:
-        config = Config(**json.load(config_file))
+    with open('config.json5', 'r', encoding='utf-8') as config_file:
+        config = Config(**json5.load(config_file))
     return config
