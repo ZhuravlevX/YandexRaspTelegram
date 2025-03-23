6cbc8f57859e47b6322a94b64e7901ab64333556
ZhuravlevX <danzhuravlevx@gmail.com>
2025-03-23 23:55:25 +0300

    The feedback system has been updated

--- main.py
+++ main.py
@@ -468,22 +468,23 @@ async def feedback_command(event, state: FSMContext):
 
     keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="❌ Отменить обращение", callback_data="cancel_feedback")]
+            [InlineKeyboardButton(text="❌ | Отменить обращение", callback_data="cancel_feedback")]
         ])
 
     if isinstance(event, Message):
-        await event.answer(
+        feedback_message = await event.answer(
             "📨 <b>Пожалуйста, напишите ваше сообщение, которое будет передано создателю бота. \n\n</b>"
             "В своем сообщении вы можете рассказать об пожеланиях, обнаруженных ошибках и багах, оставить отзыв по поводу использования бота. "
             "Убедительная просьба, не писать в обратную связь всякий не связанный бред, имейте уважение.",
             parse_mode='HTML', reply_markup=keyboard)
     elif isinstance(event, CallbackQuery):
-        await event.message.answer(
+        feedback_message = await event.message.answer(
             "📨 <b>Пожалуйста, напишите ваше сообщение, которое будет передано создателю бота. \n\n</b>"
             "В своем сообщении вы можете рассказать об пожеланиях, обнаруженных ошибках и багах, оставить отзыв по поводу использования бота. "
             "Убедительная просьба, не писать в обратную связь всякий не связанный бред, имейте уважение.",
             parse_mode='HTML', reply_markup=keyboard)
 
+    await state.update_data(feedback_message_id=feedback_message.message_id, feedback_chat_id=feedback_message.chat.id)
     await state.set_state(FeedbackStates.awaiting_feedback)
 
 
@@ -491,6 +492,10 @@ async def feedback_command(event, state: FSMContext):
 async def handle_feedback(message: Message, state: FSMContext):
     feedback_text = message.text
     user = message.from_user
+    state_data = await state.get_data()
+    feedback_message_id = state_data.get('feedback_message_id')
+    feedback_chat_id = state_data.get('feedback_chat_id')
+
     feedback_message = (
         f"💌 <b>У вас появилось новое обращение от {user.full_name} (@{user.username}):</b>\n\n{feedback_text}\n\n"
         f"<b>Для ответа пользователю используйте кнопку ниже.</b>"
@@ -498,13 +503,15 @@ async def handle_feedback(message: Message, state: FSMContext):
 
     keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="✉️ Ответить пользователю", callback_data=f"reply_{user.id}")]
+            [InlineKeyboardButton(text="✉️ | Ответить пользователю", callback_data=f"reply_{user.id}")]
         ]
     )
 
     await bot.send_message(chat_id=admin_id, text=feedback_message, parse_mode='HTML', reply_markup=keyboard)
-    await message.answer("📧 <b>Спасибо за обратную связь! В течении времени вам ответит создатель бота.</b>",
-                         parse_mode='HTML')
+    await bot.edit_message_text("📧 <b>Спасибо за обратную связь! В течении времени вам ответит создатель бота.</b>",
+                                chat_id=feedback_chat_id,
+                                message_id=feedback_message_id,
+                                parse_mode='HTML')
     await state.update_data(feedback_in_progress=True)
     await state.set_state()
 
@@ -514,9 +521,9 @@ async def ask_reply_text(callback_query: CallbackQuery, state: FSMContext):
     if callback_query.message.chat.id != int(admin_id):
         return
     user_id = int(callback_query.data.split("_")[1])
-    await state.update_data(reply_user_id=user_id)
-    await bot.send_message(chat_id=admin_id, text="📝 <b>Пожалуйста, введите текст ответа пользователю:</b>",
-                           parse_mode='HTML')
+    reply_prompt_message = await bot.send_message(chat_id=admin_id, text="📝 <b>Пожалуйста, введите текст ответа пользователю:</b>",
+                                                  parse_mode='HTML')
+    await state.update_data(reply_user_id=user_id, reply_prompt_message_id=reply_prompt_message.message_id)
     await bot.delete_message(chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id)
     await state.set_state(FeedbackStates.awaiting_reply_text)
 
@@ -540,10 +547,11 @@ async def send_reply(message: Message, state: FSMContext):
     state_data = await state.get_data()
     user_id = state_data.get("reply_user_id")
     reply_text = message.text
+    reply_prompt_message_id = state_data.get('reply_prompt_message_id')
 
     keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="✅ Пометить прочитанным", callback_data="read_feedback")]
+            [InlineKeyboardButton(text="✅ | Пометить прочитанным", callback_data="read_feedback")]
         ]
     )
 
@@ -551,7 +559,10 @@ async def send_reply(message: Message, state: FSMContext):
         await bot.send_message(chat_id=user_id,
                                text=f"📩 <b>Вам поступило сообщение от создателя бота в ответ на ваше обращение:</b>\n\n{reply_text}",
                                parse_mode='HTML', reply_markup=keyboard)
-        await message.answer("📧 <b>Сообщение было успешно отправлено пользователю.</b>", parse_mode='HTML')
+        await bot.edit_message_text("📧 <b>Сообщение было успешно отправлено пользователю.</b>",
+                                    chat_id=admin_id,
+                                    message_id=reply_prompt_message_id,
+                                    parse_mode='HTML')
     except Exception as e:
         await message.answer(f"❌ <b>Не удалось отправить сообщение пользователю. Ошибка:</b> {e}", parse_mode='HTML')
     await state.set_state()
