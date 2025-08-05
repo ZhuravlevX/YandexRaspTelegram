39df35cd9640d4a77e1fc6fd228760c794f32f6a
ZhuravlevX <danzhuravlevx@gmail.com>
2025-08-05 17:20:00 +0300

    Functions are no longer nested within other functions

--- src/request/get_suburban_info.py
+++ src/request/get_suburban_info.py
@@ -5,7 +5,6 @@ from datetime import datetime, timedelta
 import pytz
 import requests
 from babel.dates import format_date
-from dotenv import load_dotenv
 
 from src.utils.load_config import load_config
 from src.models.search_response import SearchResponse
@@ -15,101 +14,105 @@ from src.utils.format_transport_subtype import format_transport_subtype
 config = load_config()
 
 
+def search_suburban(from_station: str, to_station: str, date: str, tz: str):
+    search_request = requests.get(
+        f"{os.getenv('YANDEX_API_URL')}/search?apikey={os.getenv('TOKEN_YANDEX')}&from={from_station}&to={to_station}&lang=ru_RU&date={date}&result_timezone={tz}&transport_types=suburban&limit=250"
+    )
+
+    if not search_request.ok:
+        logging.warning(f"API request error {search_request.text}")
+        return None, None
+
+    search = SearchResponse(**search_request.json())
+    suburbans = search.segments
+    info = search.search
+    return suburbans, info
+
+
+def format_suburban_info(suburbans, info, date, tz_str, from_station, to_station, express):
+    suburban_info = []
+    msg = ""
+
+    for suburban in suburbans:
+        if suburban.departure.astimezone(tz_str).date() > date.date():
+            continue
+
+        formatted_date = format_date(date, format='d MMMM', locale='ru_RU')
+        timezone_now = datetime.now(tz_str)
+
+        if timezone_now.timestamp() > suburban.departure.timestamp():
+            continue
+
+        if express:
+            if not suburban.thread.express_type == "express":
+                continue
+
+        transport_subtype = format_transport_subtype(suburban.thread.transport_subtype.title)
+        carrier = suburban.thread.carrier.title
+
+        if suburban.thread.number == "МЦК":
+            transport_subtype = "Ласточка"
+
+        emoji = config.suburban_map.get(carrier, config.suburban_map.get(transport_subtype, "🚆"))
+
+        ticket_price = "Неизвестно"
+        if suburban.tickets_info and suburban.tickets_info.places:
+            ticket_price = f'{suburban.tickets_info.places[0].price.whole} рублей'
+
+        departure_platform = suburban.departure_platform or "неизвестного пути"
+
+        duration = suburban.duration
+        hours, remainder = divmod(duration, 3600)
+        minutes, _ = divmod(remainder, 60)
+
+        if hours > 0:
+            duration_time = f'{int(hours)} час {int(minutes)} мин.'
+        else:
+            duration_time = f'{int(minutes)} мин.'
+
+        time_until_arrival = suburban.departure - timezone_now
+        hours, remainder = divmod(time_until_arrival.seconds, 3600)
+        minutes, _ = divmod(remainder, 60)
+        if hours == 0 and minutes == 0:
+            time_until_arrival_str = 'Прибывает на станцию'
+        elif hours == 0:
+            time_until_arrival_str = f'{minutes} мин.'
+        else:
+            time_until_arrival_str = f'{hours} час. {minutes} мин.'
+
+        this_suburban_info = f'{emoji} <b>{suburban.thread.number} | {suburban.thread.title}</b>\n' \
+                             f'<i>Отправляется с {departure_platform} в {suburban.departure.hour}:{suburban.departure.minute:02d}</i>\n' \
+                             f'<i>С остановками: {suburban.stops}</i>\n' \
+                             f'<i>Стоимость билета: {ticket_price}</i>\n' \
+                             f'<i>Время в пути составит: {duration_time} ({suburban.arrival.hour}:{suburban.arrival.minute:02d})</i>\n' \
+                             f'<i>{transport_subtype} | {suburban.thread.carrier.title}</i>\n' \
+                             f'<b>Время до прибытия: {time_until_arrival_str}</b>\n'
+
+        if len(msg + this_suburban_info) > 900:
+            break
+
+        suburban_info.append(this_suburban_info)
+        msg = f'🗓 <b>Расписание пригородных поездов «{info.from_.title}» ({get_weather_code(from_station)}) – «{info.to.title}» ({get_weather_code(to_station)}) на {formatted_date}</b>\n\n' \
+              + '\n'.join(suburban_info)
+    return msg if suburban_info else None
+
+
 def get_suburban_info(from_station: str, to_station: str, tz: str, express: bool) -> str | None:
     tz_str = pytz.timezone(str(tz))
-    date = datetime.now(tz_str).strftime('%Y-%m-%d')
+    date = datetime.now(tz_str)
+    date_str = date.strftime('%Y-%m-%d')
 
-    def search_suburban(date):
-        search_request = requests.get(
-            f"{os.getenv('YANDEX_API_URL')}/search?apikey={os.getenv('TOKEN_YANDEX')}&from={from_station}&to={to_station}&lang=ru_RU&date={date}&result_timezone={tz}&transport_types=suburban&limit=250"
-        )
-
-        if not search_request.ok:
-            logging.warning(f"API request error {search_request.text}")
-            return None, None
-
-        search = SearchResponse(**search_request.json())
-        suburbans = search.segments
-        info = search.search
-        return suburbans, info
-
-    def format_suburban_info(suburbans, date, tz_str):
-        suburban_info = []
-        msg = ""
-
-        for suburban in suburbans:
-            if suburban.departure.astimezone(tz_str).date() > date.date():
-                continue
-
-            formatted_date = format_date(date, format='d MMMM', locale='ru_RU')
-            timezone_now = datetime.now(tz_str)
-
-            if timezone_now.timestamp() > suburban.departure.timestamp():
-                continue
-
-            if express:
-                if not suburban.thread.express_type == "express":
-                    continue
-
-            transport_subtype = format_transport_subtype(suburban.thread.transport_subtype.title)
-            carrier = suburban.thread.carrier.title
-
-            if suburban.thread.number == "МЦК":
-                transport_subtype = "Ласточка"
-
-            emoji = config.suburban_map.get(carrier, config.suburban_map.get(transport_subtype, "🚆"))
-
-            ticket_price = "Неизвестно"
-            if suburban.tickets_info and suburban.tickets_info.places:
-                ticket_price = f'{suburban.tickets_info.places[0].price.whole} рублей'
-
-            departure_platform = suburban.departure_platform or "неизвестного пути"
-
-            duration = suburban.duration
-            hours, remainder = divmod(duration, 3600)
-            minutes, _ = divmod(remainder, 60)
-
-            if hours > 0:
-                duration_time = f'{int(hours)} час {int(minutes)} мин.'
-            else:
-                duration_time = f'{int(minutes)} мин.'
-
-            time_until_arrival = suburban.departure - timezone_now
-            hours, remainder = divmod(time_until_arrival.seconds, 3600)
-            minutes, _ = divmod(remainder, 60)
-            if hours == 0 and minutes == 0:
-                time_until_arrival_str = 'Прибывает на станцию'
-            elif hours == 0:
-                time_until_arrival_str = f'{minutes} мин.'
-            else:
-                time_until_arrival_str = f'{hours} час. {minutes} мин.'
-
-            this_suburban_info = f'{emoji} <b>{suburban.thread.number} | {suburban.thread.title}</b>\n' \
-                                 f'<i>Отправляется с {departure_platform} в {suburban.departure.hour}:{suburban.departure.minute:02d}</i>\n' \
-                                 f'<i>С остановками: {suburban.stops}</i>\n' \
-                                 f'<i>Стоимость билета: {ticket_price}</i>\n' \
-                                 f'<i>Время в пути составит: {duration_time} ({suburban.arrival.hour}:{suburban.arrival.minute:02d})</i>\n' \
-                                 f'<i>{transport_subtype} | {suburban.thread.carrier.title}</i>\n' \
-                                 f'<b>Время до прибытия: {time_until_arrival_str}</b>\n'
-
-            if len(msg + this_suburban_info) > 900:
-                break
-
-            suburban_info.append(this_suburban_info)
-            msg = f'🗓 <b>Расписание пригородных поездов «{info.from_.title}» ({get_weather_code(from_station)}) – «{info.to.title}» ({get_weather_code(to_station)}) на {formatted_date}</b>\n\n' + '\n'.join(
-                suburban_info)
-        return msg if suburban_info else None
-
-    suburbans, info = search_suburban(date)
+    suburbans, info = search_suburban(from_station, to_station, date_str, tz)
     if suburbans is None:
         return None
 
-    msg = format_suburban_info(suburbans, datetime.now(tz_str), tz_str)
+    msg = format_suburban_info(suburbans, info, date, tz_str, from_station, to_station, express)
     if not msg:
-        next_day = datetime.now(tz_str) + timedelta(days=1)
-        suburbans, info = search_suburban(next_day.strftime('%Y-%m-%d'))
+        next_day = date + timedelta(days=1)
+        next_day_str = next_day.strftime('%Y-%m-%d')
+        suburbans, info = search_suburban(from_station, to_station, next_day_str, tz)
         if suburbans is None:
             return None
-        msg = format_suburban_info(suburbans, next_day, tz_str)
+        msg = format_suburban_info(suburbans, info, next_day, tz_str, from_station, to_station, express)
 
     return msg
--- src/request/get_train_info.py
+++ src/request/get_train_info.py
@@ -13,88 +13,92 @@ from src.models.search_response_train import SearchResponse
 config = load_config()
 
 
+def search_trains(from_city: str, to_city: str, date: str, tz: str):
+    search_request = requests.get(
+        f"{os.getenv('YANDEX_API_URL')}/search?apikey={os.getenv('TOKEN_YANDEX')}&from={from_city}&to={to_city}&lang=ru_RU&date={date}&result_timezone={tz}&transport_types=train&limit=250"
+    )
+
+    if not search_request.ok:
+        logging.warning(f"API request error {search_request.text}")
+        return None, None
+
+    search = SearchResponse(**search_request.json())
+    trains = search.segments
+    info = search.search
+    return trains, info
+
+
+def format_train_info(trains, info, date, tz_str):
+    train_info = []
+    msg = ""
+
+    for train in trains:
+        formatted_date = format_date(date, format='d MMMM', locale='ru_RU')
+        timezone_now = datetime.now(tz_str)
+
+        if train.departure.astimezone(tz_str).date() > date.date():
+            continue
+
+        if timezone_now.timestamp() > train.departure.timestamp():
+            continue
+
+        emoji = config.numbers_trains_maps_emojis.get(train.thread.number, "🚂")
+        transport_subtype = config.numbers_trains_maps_title.get(train.thread.number, "Поезд дальнего следования")
+
+        duration = train.duration
+        days, remainder = divmod(duration, 86400)
+        hours, remainder = divmod(remainder, 3600)
+        minutes, _ = divmod(remainder, 60)
+
+        if days > 0:
+            duration_time = f'{int(days)} д. {int(hours)} час {int(minutes)} мин.'
+        elif hours > 0:
+            duration_time = f'{int(hours)} час {int(minutes)} мин.'
+        else:
+            duration_time = f'{minutes} мин.'
+
+        time_until_arrival = train.departure - timezone_now
+        hours, remainder = divmod(time_until_arrival.seconds, 3600)
+        minutes, _ = divmod(remainder, 60)
+        if hours == 0 and minutes == 0:
+            time_until_arrival_str = 'Отправляться от вокзала или пункта остановки'
+        elif hours == 0:
+            time_until_arrival_str = f'{minutes} мин.'
+        else:
+            time_until_arrival_str = f'{hours} час {minutes} мин.'
+
+        this_train_info = f'{emoji} <b>{train.thread.number} | {train.thread.title}</b>\n' \
+                          f'<i>Отправляется с {train.from_.title} в {train.departure.hour}:{train.departure.minute:02d} по местному времени</i>\n' \
+                          f'<i>Прибудет в {train.to.title} в {train.arrival.hour}:{train.arrival.minute:02d} по местному времени</i>\n' \
+                          f'<i>Время в пути составит: {duration_time}</i>\n' \
+                          f'<i>{transport_subtype} | {train.thread.carrier.title}</i>\n' \
+                          f'<b>Время до отправления: {time_until_arrival_str}</b>\n'
+
+        if len(msg + this_train_info) > 900:
+            break
+
+        train_info.append(this_train_info)
+        msg = f'🗓 <b>Расписание поездов дальнего следования «{info.from_.title}» ({get_weather_title(info.from_.title)}) – «{info.to.title}» ({get_weather_title(info.to.title)}) на {formatted_date}</b>\n\n' + '\n'.join(
+            train_info)
+    return msg if train_info else None
+
+
 def get_train_info(from_city: str, to_city: str, tz: str) -> str | None:
     tz_str = pytz.timezone(str(tz))
-    date = datetime.now(tz_str).strftime('%Y-%m-%d')
+    date = datetime.now(tz_str)
+    date_str = date.strftime('%Y-%m-%d')
 
-    def search_trains(date):
-        search_request = requests.get(
-            f"{os.getenv('YANDEX_API_URL')}/search?apikey={os.getenv('TOKEN_YANDEX')}&from={from_city}&to={to_city}&lang=ru_RU&date={date}&result_timezone={tz}&transport_types=train&limit=250"
-        )
-
-        if not search_request.ok:
-            logging.warning(f"API request error {search_request.text}")
-            return None, None
-
-        search = SearchResponse(**search_request.json())
-        trains = search.segments
-        info = search.search
-        return trains, info
-
-    def format_train_info(trains, date, tz_str):
-        train_info = []
-        msg = ""
-
-        for train in trains:
-            formatted_date = format_date(date, format='d MMMM', locale='ru_RU')
-            timezone_now = datetime.now(tz_str)
-
-            if train.departure.astimezone(tz_str).date() > date.date():
-                continue
-
-            if timezone_now.timestamp() > train.departure.timestamp():
-                continue
-
-            emoji = config.numbers_trains_maps_emojis.get(train.thread.number, "🚂")
-            transport_subtype = config.numbers_trains_maps_title.get(train.thread.number, "Поезд дальнего следования")
-
-            duration = train.duration
-            days, remainder = divmod(duration, 86400)
-            hours, remainder = divmod(remainder, 3600)
-            minutes, _ = divmod(remainder, 60)
-
-            if days > 0:
-                duration_time = f'{int(days)} д. {int(hours)} час {int(minutes)} мин.'
-            elif hours > 0:
-                duration_time = f'{int(hours)} час {int(minutes)} мин.'
-            else:
-                duration_time = f'{minutes} мин.'
-
-            time_until_arrival = train.departure - timezone_now
-            hours, remainder = divmod(time_until_arrival.seconds, 3600)
-            minutes, _ = divmod(remainder, 60)
-            if hours == 0 and minutes == 0:
-                time_until_arrival_str = 'Отправляться от вокзала или пункта остановки'
-            elif hours == 0:
-                time_until_arrival_str = f'{minutes} мин.'
-            else:
-                time_until_arrival_str = f'{hours} час {minutes} мин.'
-
-            this_train_info = f'{emoji} <b>{train.thread.number} | {train.thread.title}</b>\n' \
-                              f'<i>Отправляется с {train.from_.title} в {train.departure.hour}:{train.departure.minute:02d} по местному времени</i>\n' \
-                              f'<i>Прибудет в {train.to.title} в {train.arrival.hour}:{train.arrival.minute:02d} по местному времени</i>\n' \
-                              f'<i>Время в пути составит: {duration_time}</i>\n' \
-                              f'<i>{transport_subtype} | {train.thread.carrier.title}</i>\n' \
-                              f'<b>Время до отправления: {time_until_arrival_str}</b>\n'
-
-            if len(msg + this_train_info) > 900:
-                break
-
-            train_info.append(this_train_info)
-            msg = f'🗓 <b>Расписание поездов дальнего следования «{info.from_.title}» ({get_weather_title(info.from_.title)}) – «{info.to.title}» ({get_weather_title(info.to.title)}) на {formatted_date}</b>\n\n' + '\n'.join(
-                train_info)
-        return msg if train_info else None
-
-    trains, info = search_trains(date)
+    trains, info = search_trains(from_city, to_city, date_str, tz)
     if trains is None:
         return None
 
-    msg = format_train_info(trains, datetime.now(tz_str), tz_str)
+    msg = format_train_info(trains, info, date, tz_str)
     if not msg:
-        next_day = datetime.now(tz_str) + timedelta(days=1)
-        trains, info = search_trains(next_day.strftime('%Y-%m-%d'))
+        next_day = date + timedelta(days=1)
+        next_day_str = next_day.strftime('%Y-%m-%d')
+        trains, info = search_trains(from_city, to_city, next_day_str, tz)
         if trains is None:
             return None
-        msg = format_train_info(trains, next_day, tz_str)
+        msg = format_train_info(trains, info, next_day, tz_str)
 
     return msg
